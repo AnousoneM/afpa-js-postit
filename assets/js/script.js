49a5c22936ac566f076f5a8519adf92146235876
Anousone M. <anousone.mounivongs@gmail.com>
2024-12-12 14:15:13 +0100

    fonctionnalités ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,40 @@
+// ajoput d'un écouteur d'événement sur le bouton "saveButton"
 document.querySelector('#saveButton').addEventListener('click', function () {
-    console.log('post-it sauvegardé');
+
+    // récupération des valeurs des inputs
+    let titre = document.querySelector('#titre').value;
+    let commentaires = document.querySelector('#commentaires').value;
+
+    // appel de la fonction permettant d'ajouter un post it
+    addPostIt(titre, commentaires);
+
+    // appel de la fonction permettant de vider les champs
+    effacer();
+    
 });
+
+// fonction permettant de supprimer le post it : nous recupérerons l'élément cliqué
+function supprimer(element) {
+    // nous remontons dans l'arborescence pour supprimer le parent du parent de l'élément cliqué
+    element.parentElement.parentElement.remove();
+}
+
+// fonction permettant de vider les champs titre et commentaires
+function effacer()  {
+    document.querySelector('#titre').value = '';
+    document.querySelector('#commentaires').value = '';
+}
+
+// fonction permettant d'ajouter un post it avec les valeurs des inputs "titre et commentaires" recupérées
+function addPostIt(titre, commentaires) {
+    document.querySelector('#board').innerHTML += `
+        <div class="col p-2 m-3 text-center border border-secondary-subtle border-start-0 border-top-0 bg-warning-subtle shadow">
+            <div class="text-center">
+                <i class="bi bi-pin-angle-fill text-danger-subtle fs-3"></i>
+                <i class="bi bi-x-lg float-end" onclick="supprimer(this)"></i>
+            </div>
+            <p class="h4">${titre}</p>
+            <p>${commentaires}</p>
+        </div>
+    `
+}
